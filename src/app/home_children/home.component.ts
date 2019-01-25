101edbac88a4e771c7d496add7e9a8a0783ea51a
Daniel Burbach <daniel@>
2019-01-25 12:40:06 -0800

    add some html to show an error on the childrens pages. work on main page to select parents or children

--- src/app/home_children/home.component.ts
+++ src/app/home_children/home.component.ts
@@ -8,14 +8,10 @@ import { NgModule } from '@angular/core';
 import { routing } from './app.routing'
 
 // component imports
-import { WelcomeComponent } from './welcome/welcome.component';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 import { VideosComponent } from './videos/videos.component';
 import { GamesComponent } from './games/games.component';
 import { CharactersComponent } from './characters/characters.component';
 import { SetsComponent } from './sets/sets.component';
-import { AboutComponent } from './about/about.component';
 
 @Component({
   selector: 'app-home',
--- src/app/welcome_parents/welcome.component.ts
+++ src/app/welcome_parents/welcome.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ShopParentsComponent } from './shop-parents/shop-parents.component';
+import { ProductsParentsComponent } from './products-parents/products-parents.component';
+import { SupportParentsComponent } from './support-parents/support-parents.component';
+import { GamesParentsComponent } from './games-parents/games-parents.component';
+import { LegoLifeParentsComponent } from './lego-life-parents/lego-life-parents.component';
+import { AboutComponent } from './about/about.component';
+
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
